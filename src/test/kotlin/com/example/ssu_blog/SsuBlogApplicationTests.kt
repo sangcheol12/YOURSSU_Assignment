806e6cfe750358a5f0ead8223b55e9e961121041
sangcheol12 <eric2057@daum.net>
2023-09-11 15:09:08 +0900

    create repo
    create yourssu assignment repo

---
+++ src/test/kotlin/com/example/ssu_blog/SsuBlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ssu_blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SsuBlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
